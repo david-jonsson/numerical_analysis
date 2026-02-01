b983f179b83e5feecc015c278d30bc1865f34238
David Jonsson <david.jonsson@atlascopco.com>
2026-02-01 15:35:15 +0100

    added some documentation

--- lagrange_p.py
+++ lagrange_p.py
@@ -6,19 +6,29 @@ def get_factor(x, x_i, x_j):
 
 def get_basis_p(x, i, points):
     basis_p = 1
+
     for j in range (0, len(points)):
         if (i == j): continue
         basis_p *= get_factor(x, points[i], points[j])
+    
     return basis_p
 
 def get_lagrange_p(step_size, points_x, points_y):
+    """
+    Computes the [len(points_x)]th order lagrange polynomial
+    
+    :param step_size:   step size used for sampling the lagrange polynomial
+    :param points_x:    x coordinates
+    :param points_y:    y coordinates
+    """
     entries = int(math.fabs(points_x[-1] - points_x[0]) / step_size) + 1
     f = np.zeros((int(entries),2))
 
     for n in range(0, entries):
-        lagrange_p = 0
-        x = n * step_size + points_x[0]
+        lagrange_p  = 0
+        x           = n * step_size + points_x[0]
         for i in range(0, len(points_x)):
             lagrange_p += points_y[i] * get_basis_p(x, i, points_x)
         f[n] = (x, lagrange_p)
+    
     return f
--- simpsons.py
+++ simpsons.py
@@ -1,16 +1,22 @@
-def integrate(f, segments, b, a):
-    if segments % 2:
-        print("need ever number of segments")
+def integrate(f, samples, b, a):
+    """
+    Definite integral of f over a to b, using composite simpsons
+    
+    :param f:       the function to integrate 
+    :param samples: nr of samples used in interpolation
+    :param b:       upper bound
+    :param a:       lower bound
+    """
+    if samples % 2:
+        print("need even number of samples")
         return
     
-    h =  (b - a) / (segments)
-    n = segments + 1
+    h   = (b - a) / (samples)
+    n   = samples + 1
     sum = 0
 
     for i in range(1, int(n) - 1):
-        if i % 2:
-            sum += 4 * f(i * h + a)
-        else:
-            sum += 2 * f(i * h + a)
+        if i % 2:   sum += 4 * f(i * h + a)
+        else:       sum += 2 * f(i * h + a)
 
     return (f(a) + sum + f(b)) * (h / 3)
