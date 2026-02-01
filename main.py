e7d8a193f137ec97f69ee0047eac048dc765ae4a
David Jonsson <david.jonsson@atlascopco.com>
2026-02-01 11:54:46 +0100

    added simpsons rule

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from lagrange_p import get_lagrange_p   
 import matplotlib.pyplot as plt
 import numpy as np
+import lagrange_p as lag   
+import simpsons as sim
+import math
+
+def f(x):
+    return math.sin(21 * x) * math.e ** ((-0.3) * x) + x
+
+print(sim.integrate(f, 1000, math.pi, 0))
 
 p_f = np.array([[-3, 2], [-2.5, 1], [-2, 0.65], [0, 0.5], [2, 0.65], [2.5, 1], [3, 2]])
 p_g = np.array([[-3, 2], [-2.5, 1.5], [-2, 1.2], [0, 1], [2, 1.2], [2.5, 1.5], [3, 2]])
 step_size = 0.1
 
-f = get_lagrange_p(step_size, p_f[:,0], p_f[:,1])
-g = get_lagrange_p(step_size, p_g[:,0], p_g[:,1])
+f = lag.get_lagrange_p(step_size, p_f[:,0], p_f[:,1])
+g = lag.get_lagrange_p(step_size, p_g[:,0], p_g[:,1])
 
 plt.plot(f[:,0], f[:,1], color='red', label='f')
 plt.plot(g[:,0], g[:,1], color='blue', label='G')
---
+++ simpsons.py
@@ -0,0 +1,16 @@
+def integrate(f, segments, b, a):
+    if segments % 2:
+        print("need ever number of segments")
+        return
+    
+    h =  (b - a) / (segments)
+    n = segments + 1
+    sum = 0
+
+    for i in range(1, int(n) - 1):
+        if i % 2:
+            sum += 4 * f((a + i) * h)
+        else:
+            sum += 2 * f((a + i) * h)
+
+    return (f(a) + sum + f(b)) * (h / 3)
